80d5a511dc44add5c2d09dde0dec9f7fd33ebb25
Rickyxrc <81732554+Rickyxrc@users.noreply.github.com>
2023-03-03 08:16:29 +0800

    Create rickyxrc.cpp

---
+++ rickyxrc.cpp
@@ -0,0 +1,7 @@
+Person Rickyxrc;
+Rickyxrc.age = 14;
+Rickyxrc.school = "Chengdu Foreign Language School,Xinjin Campus.";
+Rickyxrc.grade = floor(Grade("Senior",1));
+Rickyxrc.mail = rickyxrc.name + eval("at") + "outlook" + eval("dot") + "com";
+Rickyxrc.website = "https://rickyxrc.cc";
+Rickyxrc.dream = "To make this world better";
